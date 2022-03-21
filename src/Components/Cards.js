af845cd650ac2809b85fb202adc73e01abb4f0f9
jimcaldarise <jim.caldarise@gmail.com>
2022-03-20 22:16:14 -0700

    Update Card Images

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -11,19 +11,19 @@ function Cards() {
           <div className='cards__wrapper'>
              <ul className='cards__items'>
                 <CardItem 
-                src="images/roadtripper.png"
+                src="/images/roadtripper.png"
                 title="RoadTripper Playlist"
                 label="JavaScript, JSX, CSS, React, Node.js"
                 text="A playlist maker app which uses Spotify's API to authenticate an account, and search for songs using the user's current location. Capable of adding songs to a playlist and saving that playlist back to the user's account."
                 path="/project1"/>
                 <CardItem 
-                src="videos/calculatorvideo.mov"
+                src="/videos/calculatorvideo.mov"
                 title="Symbolic Algebra Calculator"
                 label="C++"
                 text="A calculator which parses user input using a tokenizer class, employs prefix notation to build binary trees which simplify algebraic equations subsituting user-defined variable values."
                 path="/project2"/>
                 <CardItem 
-                src="images/swissrail.png"
+                src="/images/swissrail.png"
                 title="Swiss Rail Costs"
                 label="C++"
                 text="A shortest path calculator using Dijkstra's algorithm to approximate the cheapest path between 35 towns and cities in Switzerland (and major international connections). Calculates the lowest cost path and displays the total cost and stops along the way."
